a29b63b1a291b4dd276b458f7279ee9c4aa52308
Madelief16 <57255412+Madelief16@users.noreply.github.com>
2021-12-22 14:02:07 +0100

    Training progress

--- boter kaas.py
+++ boter kaas.py
@@ -1,4 +1,6 @@
-from bke import MLAgent, is_winner, opponent, train, load, start, save, validate, RandomAgent, plot_validation
+import random
+
+from bke import MLAgent, is_winner, opponent, train, load, start, save, validate, RandomAgent, plot_validation, train_and_plot
  
  
 class MyAgent(MLAgent):
@@ -10,14 +12,22 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
+    
+random.seed(1)
    
 my_agent = MyAgent()
-train(my_agent, 3000)
-save(my_agent, 'MyAgent_3000')
+random_agent = RandomAgent()
+#train(my_agent, 3000)
+#save(my_agent, 'MyAgent_3000')
+train_and_plot(
+agent=my_agent,
+validation_agent=random_agent,
+iterations=50,
+trainings=100,
+validations=1000)
 
-
-my_agent = load('MyAgent_3000')
-my_agent.learning = False
+#my_agent = load('MyAgent_3000')
+#my_agent.learning = False
  
 #start(player_x=my_agent)
 
