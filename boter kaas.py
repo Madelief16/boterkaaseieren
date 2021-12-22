eb93ef760614ca1875b7069fa2cbe29379847633
Madelief16 <57255412+Madelief16@users.noreply.github.com>
2021-12-22 10:39:35 +0100

    validatieresultaten plotten

--- boter kaas.py
+++ boter kaas.py
@@ -1,4 +1,4 @@
-from bke import MLAgent, is_winner, opponent, train, load, start, save
+from bke import MLAgent, is_winner, opponent, train, load, start, save, validate, RandomAgent, plot_validation
  
  
 class MyAgent(MLAgent):
@@ -10,7 +10,7 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
-    
+   
 my_agent = MyAgent()
 train(my_agent, 3000)
 save(my_agent, 'MyAgent_3000')
@@ -23,7 +23,7 @@ my_agent.learning = False
  
 start(player_x=my_agent)
 
-winners = defaultdict(int)  
+winners = defaultdict(int)
 validation_agent = RandomAgent()
 for i in range(100):
     winner = start(player_x=my_agent, player_o=validation_agent, ui=HEADLESS)
@@ -31,3 +31,7 @@ for i in range(100):
 winners[PLAYER_X] = winners[PLAYER_X] / 100
 winners[PLAYER_O] = winners[PLAYER_O] / 100
 winners[None] = winners[None] / 100
+
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+ 
+plot_validation(validation_result)
