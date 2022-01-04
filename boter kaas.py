39fc4313c3eb6234189cb41ce51327a99888df0f
Madelief16 <57255412+Madelief16@users.noreply.github.com>
2022-01-04 12:34:05 +0100

    # verwijderd

--- boter kaas.py
+++ boter kaas.py
@@ -12,6 +12,9 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
+
+my_agent = MyAgent(alpha=0.2, epsilon=0.8)
+
     
 print("1: train de computer \n 2: speel tegen een getrainde computer \n 3: je kan tegen ander persoon \n 4:kijk hoe goed die het doet \n 5: Wat zijn hyperparameters? \n Kies wat je wilt spelen:")
 choice = input()
@@ -23,8 +26,8 @@ if choice == '1':
     
 if choice == '2':
     #print("Tegenover welke agent zou je willen spelen? Op het moment dat je tegen de agent van dit programma wil spelen, vul in: agent1")
- #  train(my_agent, 3000)
-    #save(my_agent, 'MyAgent_3000')
+    train(my_agent, 3000)
+    save(my_agent, 'MyAgent_3000')
 
     my_agent = load('MyAgent_3000')
     my_agent.learning = False
@@ -34,42 +37,19 @@ if choice == '3':
     start()
     
 if choice == '4':
-    print("Hoe heet de agent")
-    name = input()
-    print("Wil je dat jouw agent x of o is? (x begint altijd)")
-    symbol = input()
-    
     train(my_agent, 10000)
     save(my_agent, 'MyAgent_noBeat')
     my_agent = load('MyAgent_noBeat')
     my_agent.learning = False
-    
-    validation_result = validate(agent_o=my_agent, agent_x=validation_agent, iterations=100)
-   
-    plot_validation(validation_result)
- 
+     
     validation_agent = RandomAgent()
+     
+    validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=10000)
+     
+    plot_validation(validation_result)
     
 if choice == '5':
     print("Bij machine learning is een hyperparameter een parameter waarvan de waarde wordt gebruikt om het leerproces te regelen. Daarentegen worden de waarden van andere parameters (meestal knoopgewichten) afgeleid via training.")
   
-  #  if symbol == "x" or symbol == "X":
-   # validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
 
-    #if symbol == "o" or symbol == "O":
-
-
- 
-    
-    
-#random.seed(1)
-#my_agent = MyAgent()
-   
-#random_agent = RandomAgent()
-
-#my_agent = MyAgent(alpha=0.2, epsilon=0.8)
-
-
-
-# validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
  
